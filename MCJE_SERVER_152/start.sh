2383954dca0540c23fc596cf14763db5489cd885
“sunnychon” <chon.aimo@gmail.com>
2022-12-24 19:36:15 +0800

    add files

---
+++ MCJE_SERVER_152/start.sh
@@ -0,0 +1,3 @@
+java -Xmx5G -Xms5G -jar mc152.jar
+echo minecraft server exited
+sleep 3
---
+++ install.sh
@@ -0,0 +1,6 @@
+echo installing MINECRAFT JAVA SERVER 1.5.2
+mkdir /home/MCJE_SERVER_152
+echo copying files
+cp -r ./MCJE_SERVER_152 /home/MCJE_SERVER_152
+echo install completed!
+sleep 3
